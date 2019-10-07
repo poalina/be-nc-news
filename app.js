81399dceb100a13af95d8928eb2fb5e4420486b1
poalina <paula828@op.pl>
2019-10-07 16:43:24 +0100

    installed cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const apiRouter = require("./routes/api-router");
 const {
   handleCustomErrors,
@@ -9,6 +10,7 @@ const {
   handle405Error
 } = require("./errors/errors");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
